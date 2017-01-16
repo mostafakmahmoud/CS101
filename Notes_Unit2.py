d407358787513dbe43d151837cb9a2552e6f25bd
mostafakmahmoud <mostafa.khaled0@gmail.com>
2017-01-16 17:51:42 +0300

    16/1/2017
    Office

--- Notes_Unit2.py
+++ Notes_Unit2.py
@@ -1,29 +1,44 @@
 # HOW TO REPEAT
 #PROCEDURES and CONTROL
+#def - while - if
 
-#def <name>(<parameters>):
-#    <BLOCK>
-
-# if <TestExpression>
-#   <BLOCK>
-
-# while <TestExpression>:
-#   <BLOCK>
-
-#Example of function
-def get_next_target (S):
-    start_link = S.find('<a href=')
-    start_quote = S.find('"', start_link)
-    end_quote = S.find('"', start_quote+1)
-    url = S [start_quote+1 : end_quote]
-    return url, end_quote
-
-#Example function to find the bigger of two numbers.
+#Function to find the bigger of two numbers:
 def bigger(x,y):
     if x<y:
         return y
     else:
         return x
+
+#Function to find the smaller of two numbers:
+def smaller(a,b):
+    if a < b:
+        return a
+    else:
+        return b
+
+#Function to find the biggest of three numbers:
+def biggest (a,b,c):
+    return bigger(a,bigger(b,c))
+
+#Functions to get the Median from three numbers:
+def median1(x,y,z):
+    q = bigger(x,y)
+    w = bigger(y,z)
+    e = smaller(q,w)
+
+    if e>x and e>z:
+        return bigger(x,z)
+    else:
+        return e
+
+def median2(a,b,c):
+    big = biggest(a,b,c)
+    if big == a:
+        return bigger(b,c)
+    if big == b:
+        return bigger(a,c)
+    else:
+        return bigger(a,b)
 
 # Example function to decide if a name begins with D,N, or Else
 def is_friend (name):
--- Test.py
+++ Test.py
@@ -1,54 +1,30 @@
-page = ('<div id="top_bin"><div id="top_content" class="width960">'
-'<div class="udacity float-left"><a href="http://udacity.com">')
-
-def get_next_target (S):
+def get_next_target(S):
     start_link = S.find('<a href=')
+    if start_link == -1:
+        return None, 0
     start_quote = S.find('"', start_link)
     end_quote = S.find('"', start_quote+1)
     url = S [start_quote+1 : end_quote]
     return url, end_quote
 
-def rest_of_string (s):
-    return s[1:]
+def print_all_links(x):
+    while True:
+        url, endpos = get_next_target(x)
+        if url:
+            print url
+            x = x[endpos:]
+        else:
+            break
 
-def find_second(search, target):
-    return search.find(target, search.find(target)+1)
-# print find_second('This is Strange Strange','is')
-
-def absolute(x):
-    if x<0:
-        x = -x
+def find_last(text, target):
+    while True:
+        result = text.find(target)
+        if result == -1:
+            break
+        else:
+            x = result
+            text.find(target, result+1)
+            text = text[result+1:]
     return x
 
-def bigger(x,y):
-    if x<y:
-        return y
-    else:
-        return x
-# print bigger(4,2)
-
-def biggest (x,y,z):
-    if x>y and x>z:
-        return x
-    elif y>x and y>z:
-        return y
-    else:
-         return z
-#print biggest(6,2,3)
-#print biggest(6,2,7)
-#print biggest(6,9,3)
-
-def print_numbers(x):
-        y = 1
-        while y<=x:
-            print y
-            y = y+1
-
-def factorial(n):
-    m = 1
-    while n>0:
-        m = m*n
-        n = n-1
-    return m
-
-print factorial(52)
+print find_last('aaaa', 'a')
