185092ad105ecff704fdf37cb9575500911208b6
mostafakmahmoud <mostafa.khaled0@gmail.com>
2017-02-09 07:29:33 -0800

    9 February 2017

--- Test.py
+++ Test.py
@@ -1,25 +1,3 @@
-#sample nested list
-mylist = [
-['key1',['url1','url2']],
-['key2',['url3','url4']],
-['key3',['url5','url6']]
-]
-
-alphabet = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108,
- 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]
-
-cap = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
- 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
-
-def make_hashtable(nbuckets):
-    hashtable = []
-    i = 0
-    while i < nbuckets:
-        hashtable.append([])
-        i += 1
-    return hashtable
-print make_hashtable(5)
-
 def test_hash_function(func, keys, size):
     results = [0] * size
     keys_used = []
@@ -30,11 +8,51 @@ def test_hash_function(func, keys, size):
             keys_used.append(w)
     return results
 
-def bad_hash_string(keyword, buckets):
-    return ord(keyword[0]%buckets)
+def hashtable_get_bucket(htable, key):
+    return htable[hash_string(key, len(htable))]
 
 def hash_string(keyword, buckets):
     total = 0
     for letter in keyword:
         total = total + ord(letter)
     return total%buckets
+
+def make_hashtable(nbuckets):
+    table = []
+    for i in range(0, nbuckets):
+        table.append([])
+    return table
+
+def hashtable_add(htable,key,value):
+    #gets the position and appends the word at that position.
+    position = hash_string(key,len(htable))
+    htable[position].append([key,value])
+    return htable
+    #gets bucket then appends the word to it.
+    ##hasthable_get_bucket(htable, key).append([key, value])
+
+def hashtable_lookup(htable, key):
+    bucket = hashtable_get_bucket(htable, key)
+    for entry in bucket:
+        if key in entry:
+            return entry[1]
+    return None
+
+
+table = [
+[['Ellis', 11], ['Francis', 13]]
+, [],
+[['Bill', 17], ['Zoe', 14]],
+[['Coach', 4]],
+[['Louis', 29], ['Nick', 2], ['Rochelle', 4]]]
+
+print hashtable_lookup(table, 'Francis')
+#>>> 13
+
+print hashtable_lookup(table, 'Louis')
+#>>> 29
+
+print hashtable_lookup(table, 'Zoe')
+#>>> 14
+
+print hashtable_lookup(table, 'jjj')
--- scratchpad.py
+++ scratchpad.py
@@ -1,7 +1,24 @@
-def make_hashtable(nbuckets):
-    table = []
-    for i in range(0, nbuckets):
-        table.append([])
-    return table
+alphabet = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108,
+ 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]
 
-print make_hashtable(3)
+cap = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+ 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
+
+#sample nested list
+mylist = [
+['key1',['url1','url2']],
+['key2',['url3','url4']],
+['key3',['url5','url6']]
+]
+
+htable = [
+[['key1',['url','url','url']],['key2',['url','url','url']],['key3',['url','url']]],
+[['key4',['url','url','url']],['key5',['url','url','url']],['key6',['url','url']]],
+[['key7',['url','url','url']],['key8',['url','url','url']],['key9',['url','url']]],
+]
+
+print htable[0] #bucket
+print htable[0][1] #entry
+print htable [0][1][0] #key
+print htable [0][1][1] #urls
+print 'key' in htable [0][1][0]
