4bcef56a3f7c50a459d34b46c93d6e14570ea363
mostafakmahmoud <mostafa.khaled0@gmail.com>
2017-02-19 08:01:16 -0800

    sunday 19 Feb

---
+++ Notes_Unit6.py
@@ -0,0 +1,10 @@
+#RECURSIVE DEFINITIONS:
+#Two Parts:
+    # 1. Base Case - A Starting point
+        #Not Defined interms of itself
+        #Smallest input - we already know the answer.
+    # 2. Recursive Case
+        #Defined interms of "smaller" version of itself.
+
+#Palindromes:
+#A word read same way forwards or backwards
--- Test.py
+++ Test.py
@@ -1,65 +1,52 @@
-# 6. In video 28. Update, it was suggested that some of the duplicate code in
-# lookup and update could be avoided by a better design.  We can do this by
-# defining a procedure that finds the entry corresponding to a given key, and
-# using that in both lookup and update.
-
-# Here are the original procedures:
-
-def bucket_find(bucket, key):
-    for entry in bucket:
-        if entry[0] == key:
-            return entry
-    return None
-
-def hashtable_update(htable, key, value):
-    bucket = hashtable_get_bucket(htable, key)
-    entry = bucket_find(bucket, key)
-    if entry:
-        entry[1] == value
+#Recursive function to get factorial of a number.
+def factorial(n):
+    if n == 0:
+        return 1
     else:
-        bucket.append([key, value])
+        return n*factorial(n-1)
 
-def hashtable_lookup(htable, key):
-    entry = bucket_find(hashtable_get_bucket(htable, key), key)
-    if entry:
-        return entry[1]
+#Recursive function to test if a string is a palindrome:
+def is_palindrome(x):
+    if x == '':
+        return True
     else:
-        return None
+        if x[0] != x[-1]:
+            return False
+        else:
+            return is_palindrome(x[1:-1])
+#Iterative function to test if a string is a palindrome:
+def iter_palindrome(s):
+    for i in range90, len(s / 2):
+        if s[i] != s[-(-i+i)]:
+            return False
+    return True
 
-def make_hashtable(size):
-    table = []
-    for unused in range(0, size):
-        table.append([])
-    return table
+# fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+#print fibonacci(3)
+# Finished in 7.727 seconds
 
-def hash_string(s, size):
-    h = 0
-    for c in s:
-         h = h + ord(c)
-    return h % size
+def fibonacci2(n):
+    current = 0
+    after = 1
+    x = []
+    for i in range(0,n):
+        x.append(current)
+        current, after = after, current + after
+    return x
 
-def hashtable_get_bucket(htable, key):
-    return htable[hash_string(key, len(htable))]
+def golden_ratio(x):
+    my_list = fibonacci2(x)
+    g_ratio = []
+    for i in my_list:
+        g_ratio.append(my_list[i+1]/my_list[i])
+    return g_ratio
 
-# Whenever we have duplicate code like the loop that finds the entry in
-# hashtable_update and hashtable_lookup, we should think if there is a better way
-# to write this that would avoid the duplication. We should be able to rewrite
-# these procedures to be shorter by defining a new procedure and rewriting both
-# hashtable_update and hashtable_lookup to use that procedure.
-
-# Modify the code for both hashtable_update and hashtable_lookup to have the same
-# behavior they have now, but using fewer lines of code in each procedure.  You
-# should define a new procedure to help with this. Your new version should have
-# approximately the same running time as the original version, but neither
-# hashtable_update or hashtable_lookup should include any for or while loop, and
-# the block of each procedure should be no more than 6 lines long.
-
-# Your procedures should have the same behavior as the originals.  For example,
-
-table = make_hashtable(10)
-hashtable_update(table, 'Python', 'Monty')
-hashtable_update(table, 'CLU', 'Barbara Liskov')
-hashtable_update(table, 'JavaScript', 'Brendan Eich')
-hashtable_update(table, 'Python', 'Guido van Rossum')
-print hashtable_lookup(table, 'Python')
-#>>> Guido van Rossum
+# print fibonacci2(36)
+print golden_ratio(36)
