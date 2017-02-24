05c5a8a3fa264bda4f4a14bf678b7cc8f32bb936
mostafakmahmoud <mostafa.khaled0@gmail.com>
2017-02-24 14:04:14 -0800

    Annotations and cleanup

--- Crawler.py
+++ Crawler.py
@@ -1,10 +1,15 @@
-import urllib2
+#Basic Web Crawler program in python.
+#Course CS101 - Introduction to Computer Science
 
-def crawl_web(seed): #returns index, graph of outlinks
+import urllib2 #for function get_page
+
+def crawl_web(seed):
+    #Crawler function that starts from a seed page
+    #Returns an index of words and urls, and graph of outlinks.
     tocrawl = [seed] #pages to be crawled starting with seed page
     crawled = [] #Keeping track of crawled pages
-    index = {} #our index of pages and keywords
-    graph = {} #Urank - {url:[list of pages it links to]}
+    index = {} #Our index of pages and keywords
+    graph = {} #Urank - {url:[list of urls it links to]}
     while tocrawl: #while there are pages left in tocrawl
         page = tocrawl.pop() #get the last page from tocrawl
         if page not in crawled: #check if its in crawled pages
@@ -16,9 +21,34 @@ def crawl_web(seed): #returns index, graph of outlinks
             crawled.append(page) #adds the crawled page to crawled list.
     return index, graph
 
+def compute_ranks(graph):
+    #Ranks webpages.
+    #Rank is based on number of links leading to that page and rank of the link origin.
+
+    d = 0.8 #damping factor
+    t = 0 #time step
+    numloops = 10 #Number of times we will go through relaxation
+    ranks = {}
+    npages = len(graph) #Number of pages that we crawled
+    for pages in graph: #initialize each page.
+        ranks[page] = 1.0/ npages #maps each page to its current rank.
+    for i in range(0, numloops): #Go through the number of times of numloops.
+        newranks = {}
+        for page in graph: #update new ranks based on the formula using old ranks.
+            newrank = (1-d) / npages
+            for node in graph:
+                if page in graph[node]:
+                    newrank = newrank + d * (ranks[nodes]/ len(graph[node]))
+            newranks[page] = newrank
+        ranks = newranks #Assign new ranks to ranks at the end of each loop.
+        return ranks
+
 def get_all_links(page):
+    #Goes through a webpage, calls get_next_target until no more urls left in the page.
+    #Returns a list of urls from that page.
     list1 = []
     while True:
+        #get url and endposition, append the url to list, page starts from end position.
         url, endpos = get_next_target(page)
         if url:
             list1.append(url)
@@ -28,44 +58,52 @@ def get_all_links(page):
     return list1
 
 def get_next_target(page):
-    start_link = page.find('<a href=')
-    if start_link == -1:
+    #Extracts the next url from a page.
+    #Returns the url and where it stopped in that page.
+    start_link = page.find('<a href=') #find the start of a url
+    if start_link == -1: #if no result return none.
         return None, 0
-    start_quote = page.find('"', start_link)
-    end_quote = page.find('"', start_quote+1)
-    url = page[start_quote+1 : end_quote]
+    start_quote = page.find('"', start_link)#start of the url
+    end_quote = page.find('"', start_quote+1)#end of the url
+    url = page[start_quote+1 : end_quote]#get between start and end.
     return url, end_quote
 
 def get_page(url):
+    #returns source code for a webpage.
+    #depends on urllib2 library
     try:
         return urllib2.urlopen(url).read()
     except:
         return ""
 
 def union(p,q):
+    #Union function for lists.
     for e in q:
         if e not in p:
             p.append(e)
     return p
 
-def add_to_index(index, keyword, url):
-    #look if keyword is already in index, add url to list of urls.
-    if keyword in index:
-        index[keyword].append(url)
-    else:
-        #if not found, add new entry.
-        index[keyword] = [url]
-
-def lookup(index, keyword):
-    if keyword in index:
-        return index[keyword]
-    else:
-        return None
-
 def add_page_to_index(index,url,content):
+    #Splits content of a page into keywords (by space characters).
+    #Updates the index to include all the words found in that page.
     words = content.split()
     for word in words:
         add_to_index(index, word, url)
 
-myindex = crawl_web('https://www.udacity.com/cs101x/index.html')
-print lookup(myindex, 'Squeamish')
+def add_to_index(index, keyword, url):
+    #Checks if a keyword is already in index.
+    #Adds url to list of urls for that word.
+    if keyword in index:
+        index[keyword].append(url)
+        #if not found, create new entry for the word and url.
+    else:
+        index[keyword] = [url]
+    #Doesn't return anything. it Only updates the index.
+
+def lookup(index, keyword):
+    #Checks the index for the keyword passed.
+    #Returns list of urls associated with that keyword.
+    if keyword in index:
+        return index[keyword]
+    else:
+        return None
--- Notes_Unit6.py
+++ Notes_Unit6.py
@@ -7,31 +7,24 @@
         #Defined interms of "smaller" version of itself.
 
 #Circular vs Recursive
-#No base case in Circular, no way to stop.
+    #No base case in Circular, no way to stop.
 
 #RELAXATION ALGORITHM:
     #Start with a guess
         #while not done:
             #make the guess better
 
-#Page Rank:
-# sum of pages with links to URL /
-# number of links in that page.
+#Damping function: #more urls equals less value to each url
 
-#Damping function
-#more urls equals less value to each url
-
-#Recursive function to get factorial of a number.
 def factorial(n):
+    #Recursive function to get factorial of a number.
     if n == 0:
         return 1
     else:
         return n*factorial(n-1)
 
-#Palindromes:
-#A word read same way forwards or backwards
-#Recursive function to test if a string is a palindrome:
 def is_palindrome(x):
+    #Recursive function to test if a string is a palindrome:
     if x == '':
         return True
     else:
@@ -39,25 +32,27 @@ def is_palindrome(x):
             return False
         else:
             return is_palindrome(x[1:-1])
-#Iterative function to test if a string is a palindrome:
+
 def iter_palindrome(s):
+    #Iterative function to test if a string is a palindrome:
     for i in range90, len(s / 2):
         if s[i] != s[-(-i+i)]:
             return False
     return True
 
-#fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
-#sum of numbers in fib sequence up to specified number.
 def fibonacci(n):
+    #fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+    #sum of numbers in fib sequence up to specified number.
     if n == 0:
         return 0
     elif n == 1:
         return 1
     else:
         return fibonacci(n-1) + fibonacci(n-2)
-#Faster Iterative Fibonacci
-#returns list of Fibonacci sequence up to specified number.
+
 def fibonacci2(n):
+    #Faster Iterative Fibonacci
+    #returns list of Fibonacci sequence up to specified number.
     current = 0
     after = 1
     x = []
@@ -66,10 +61,26 @@ def fibonacci2(n):
         current, after = after, current + after
     return x
 
-#Nodes:
+#NODES:
 {url:[
 {'A':['B','C','D'],
 'B':[],
 'C':['A'],
 'D':[]
 }}
+
+#INDEX:
+A nested list of keywords and urls those keywords are found.
+mylist = [
+['key1',['url1','url2']],
+['key2',['url3','url4']],
+['key3',['url5','url6']]
+]
+
+#HASHTABLE:
+A Sample Nested list of buckets with keywords and urls.
+htable = [
+[['key1',['url','url','url']],['key2',['url','url','url']],['key3',['url','url']]],
+[['key4',['url','url','url']],['key5',['url','url','url']],['key6',['url','url']]],
+[['key7',['url','url','url']],['key8',['url','url','url']],['key9',['url','url']]],
+]
